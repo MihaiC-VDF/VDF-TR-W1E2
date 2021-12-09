aedf2a90265b320cba78efc35d8579b91e309ea8
MihaiC-VDF <mihaicucuianu@gmail.com>
2021-12-09 08:07:16 +0200

    Initial commit

---
+++ src/vdf/company/Main.java
@@ -0,0 +1,14 @@
+package vdf.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+     TestClass excersiseTest = new TestClass();
+
+        System.out.println("Printout pentru variabile, numar " + excersiseTest.getNumber() + " si caracter" + excersiseTest.getCharter());
+
+
+    }
+}
---
+++ src/vdf/company/TestClass.java
@@ -0,0 +1,20 @@
+package vdf.company;
+
+public class TestClass {
+
+    private int number;
+    private char charter;
+
+//    Empty constructor for class initialisation
+
+    public TestClass() {
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public char getCharter() {
+        return charter;
+    }
+}
